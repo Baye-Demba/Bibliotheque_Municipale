023430be682bca94a557c6d67fad7d5711819bed
Baye-Demba <gadiaghino03@gmail.com>
2026-02-22 00:42:35 +0000

    creation

---
+++ src/main/java/com/example/bibliotheque_municipale/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.bibliotheque_municipale;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
